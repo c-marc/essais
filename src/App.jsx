82c7b22fc7fa5160c5e224f8013125db3c81a8a0
Marc C <mcar176@gmail.com>
2023-05-08 16:51:52 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  Link,
+} from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+import "./App.css";
+
+const Home = () => {
+  const [picture, setPicture] = useState(null);
+
+  const handleFileChange = (event) => {
+    console.log(event.target.files[0]);
+    setPicture(event.target.files[0]);
+  };
+  return (
+    <>
+      <label htmlFor="file">File Input</label>
+      <input id="file" type="file" onChange={handleFileChange} />
+      <br />
+      <Link to="/login">Login</Link>
+      <Link to="/protected">Protected</Link>
+    </>
+  );
+};
+
+const Protected = ({ token }) => {
+  return <p>Yes: {token}</p>;
+};
+
+const Login = ({ handleToken }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    handleToken("blabla");
+    navigate("/");
+  };
+  return <button onClick={handleClick}>Login</button>;
+};
+
+function App() {
+  const [token, handleToken] = useState(null);
+
+  console.log("Test");
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/protected"
+            element={
+              token ? <Protected token={token} /> : <Navigate to="/login" />
+            }
+          />
+          <Route path="/login" element={<Login handleToken={handleToken} />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
