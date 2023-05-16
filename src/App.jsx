641ca45d1aaded42a9e9cb19507b265bd83d961a
Marc C <mcar176@gmail.com>
2023-05-16 14:12:48 +0200

    More experiments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -52,15 +52,155 @@ const Login = ({ handleToken }) => {
   return <button onClick={handleClick}>Login</button>;
 };
 
+function TestSetState() {
+  const [disabled, setDisabled] = useState(false);
+
+  const handleClick = async () => {
+    const dummy = async () => {
+      let i = 0;
+      while (i < 5 * 10 ** 9) {
+        i++;
+      }
+      return true;
+    };
+    setDisabled(true);
+    await dummy();
+    console.log("disabled", disabled);
+    console.log("react click");
+  };
+
+  return (
+    <button disabled={disabled} onClick={handleClick}>
+      Fake
+    </button>
+  );
+}
+
+function TestSetState2() {
+  const [disabled, setDisabled] = useState(false);
+  const myRef = useRef(false);
+
+  console.log("react component says state", disabled);
+
+  useEffect(() => {
+    myRef.current.addEventListener("click", () =>
+      console.log("ref says click")
+    );
+  }, []);
+
+  const handleClick = async () => {
+    const dummy = async () => {
+      let i = 0;
+      while (i < 5 * 10 ** 9) {
+        i++;
+      }
+      return true;
+    };
+    console.log("handler says click");
+    setDisabled(true);
+    console.log("handler asked for state disabled true");
+    console.log("handler says state disabled", disabled);
+    console.log("handler says ref disabled", myRef.current.disabled);
+    console.log("handler says await");
+    await dummy();
+    console.log("handler says await resolved");
+    console.log("handler says state disabled", disabled);
+    console.log("handler says ref disabled", myRef.current.disabled);
+  };
+}
+
+function TestSetState3() {
+  const [disabled, setDisabled] = useState(false);
+  const myRef = useRef(false);
+
+  console.log("react component says state", disabled);
+
+  useEffect(() => {
+    myRef.current.addEventListener("click", () =>
+      console.log("ref says click")
+    );
+  }, []);
+
+  const handleClick = () => {
+    console.log("handler says click");
+    setDisabled(true);
+    console.log("handler asked for state disabled true");
+    console.log("handler says state disabled", disabled);
+    console.log("handler says ref disabled", myRef.current.disabled);
+    console.log("handler says wait");
+    let i = 0;
+    while (i < 5 * 10 ** 9) {
+      i++;
+    }
+    console.log("handler says wait resolved");
+    console.log("handler says state disabled", disabled);
+    console.log("handler says ref disabled", myRef.current.disabled);
+  };
+
+  return (
+    <button ref={myRef} disabled={disabled} onClick={handleClick}>
+      Fake
+    </button>
+  );
+}
+
+function TestSetState4() {
+  const [disabled, setDisabled] = useState(false);
+  const myRef = useRef();
+
+  console.log("components says state disabled=", disabled);
+  console.log("components says ref disabled=", myRef.current?.disabled);
+
+  useEffect(() => {
+    console.log("useEffect says state disabled=", disabled);
+    console.log("useEffect says ref disabled=", myRef.current?.disabled);
+  }, [disabled]);
+
+  useEffect(() => {
+    myRef.current.addEventListener("click", () =>
+      console.log("ref says click")
+    );
+  }, []);
+
+  const handleClick = async () => {
+    const something = async () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log("Promise resolved");
+          resolve(true);
+        }, 5000);
+      });
+    };
+    console.log("handler says click");
+    setDisabled(true);
+    console.log("handler asked for state disabled true");
+    console.log("handler says state disabled=", disabled);
+    console.log("handler says ref disabled=", myRef.current.disabled);
+    console.log("handler says await");
+    await something();
+    console.log("handler says await resolved");
+    console.log("handler says state disabled=", disabled);
+    console.log("handler says ref disabled=", myRef.current.disabled);
+  };
+
+  return (
+    <button ref={myRef} disabled={disabled} onClick={handleClick}>
+      Fake
+    </button>
+  );
+}
+
 function App() {
   const [token, handleToken] = useState(null);
 
-  console.log("Test");
+  console.log("Test App");
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route path="/test" element={<TestSetState4 />} />
 
           <Route
             path="/protected"
